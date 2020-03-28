0b6f5dcdf7b7422677bff5ccc73fe3c0543dec95
Gonzalo <Gonzalo@Spartan>
2020-03-28 02:28:53 +0100

    Controller Tests
    Y SerialVersion Exceptions

--- src/main/java/org/springframework/samples/petclinic/service/exceptions/NoMultipleContractCommercialException.java
+++ src/main/java/org/springframework/samples/petclinic/service/exceptions/NoMultipleContractCommercialException.java
@@ -3,4 +3,8 @@ package org.springframework.samples.petclinic.service.exceptions;
 
 public class NoMultipleContractCommercialException extends Exception {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 }
