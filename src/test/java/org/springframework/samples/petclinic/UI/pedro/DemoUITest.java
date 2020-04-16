471f0efc794fe57e29802b9059b3c4dee84899d2
Pedro Manuel Cobos Ramos <pedcobram@alum.us.es>
2020-04-16 19:51:08 +0200

    Raw UI tests done, refactoring needed

--- src/test/java/org/springframework/samples/petclinic/UI/pedro/DemoUITest.java
+++ src/test/java/org/springframework/samples/petclinic/UI/pedro/DemoUITest.java
@@ -16,15 +16,22 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 public class DemoUITest {
 
 	private WebDriver		driver;
+	@SuppressWarnings("unused")
 	private String			baseUrl;
 	private boolean			acceptNextAlert		= true;
 	private StringBuffer	verificationErrors	= new StringBuffer();
@@ -89,7 +96,7 @@ public class DemoUITest {
 	public void R12Positivo_PresidenteAceptarPartido() throws Exception {
 		this.driver.get("www.localhost:" + this.port + "/?lang=es");
 		this.driver.get("www.localhost:" + this.port + "/login");
-		//this.driver.findElement(By.linkText("Conectarse")).click();
+		//this.driver.findElement(By.linkText("CONECTARSE")).click();
 		this.driver.findElement(By.id("username")).clear();
 		this.driver.findElement(By.id("username")).sendKeys("presidente1");
 		this.driver.findElement(By.id("password")).clear();
@@ -140,12 +147,232 @@ public class DemoUITest {
 		this.driver.findElement(By.id("password")).clear();
 		this.driver.findElement(By.id("password")).sendKeys("presidente1");
 		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
-		this.driver.get("http://www.localhost:" + this.port + "/matchRequests/received");
-		//this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li[2]/a/strong")).click();
-		//this.driver.findElement(By.linkText("Peticiones de partido recibidas")).click();
+		//this.driver.get("http://www.localhost:" + this.port + "/matchRequests/received");
+		this.driver.findElement(By.linkText("PRESIDENTE1")).click();
+		this.driver.findElement(By.linkText("PRESIDENTE1")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li[2]/a/strong")).click();
+		this.driver.findElement(By.linkText("PETICIONES DE PARTIDO RECIBIDAS")).click();
 		this.driver.findElement(By.xpath("(//a[contains(text(),'Aceptar')])[2]")).click();
 		this.driver.findElement(By.xpath("//table[@id='matchRequestsTable']/tbody/tr[2]/td[3]")).click();
 		Assert.assertEquals("REFUSE", this.driver.findElement(By.xpath("//table[@id='matchRequestsTable']/tbody/tr[2]/td[3]")).getText());
+	}
+
+	@Test
+	public void R13Positivo_RefereeAceptarPeticionPartido() throws Exception {
+		this.driver.get("http://localhost:" + this.port + "/?lang?es");
+		this.driver.get("http://localhost:" + this.port + "/login");
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("referee1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("referee1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		//this.driver.get("http://localhost:" + this.port + "/matchRefereeRequest/list");
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/ul/li[3]/a/span")).click();
+		this.driver.findElement(By.linkText("Aceptar")).click();
+		//this.driver.get("http://localhost:" + this.port + "/matches/referee/list");
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.linkText("MI LISTA DE PARTIDOS")).click();
+		Assert.assertEquals("Match title 3", this.driver.findElement(By.xpath("//table[@id='matchesTable']/tbody/tr[2]/td")).getText());
+	}
+
+	@Test
+	public void R14Positivo_RefereeEditarActaPartido() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.get("http://localhost:" + this.port + "/login");
+		//this.driver.findElement(By.linkText("Conectarse")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("referee1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("referee1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		//this.driver.get("http://localhost:" + this.port + "/matches/referee/list");
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/ul/li[2]/a/span")).click();
+		this.driver.findElement(By.linkText("Acta del partido")).click();
+		this.driver.findElement(By.linkText("Editar")).click();
+		this.driver.findElement(By.xpath("//form[@id='add-footballPlayer-form']/div/div")).click();
+		this.driver.findElement(By.id("title")).clear();
+		this.driver.findElement(By.id("title")).sendKeys("edited title");
+		this.driver.findElement(By.id("season_start")).clear();
+		this.driver.findElement(By.id("season_start")).sendKeys("2020");
+		this.driver.findElement(By.id("result")).clear();
+		this.driver.findElement(By.id("result")).sendKeys("edited result");
+		new Select(this.driver.findElement(By.id("status"))).selectByVisibleText("PUBLISHED");
+		this.driver.findElement(By.xpath("//option[@value='PUBLISHED']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		//this.driver.get("http://localhost:" + this.port + "/matches/referee/list");
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.linkText("MI LISTA DE PARTIDOS")).click();
+		this.driver.findElement(By.linkText("Acta del partido")).click();
+		Assert.assertEquals("edited title", this.driver.findElement(By.xpath("//b")).getText());
+	}
+
+	@Test
+	public void R14Negativo_RefereeEditarActaPartido() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.get("http://localhost:" + this.port + "/login");
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("referee1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("referee1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		//this.driver.get("http://localhost:" + this.port + "/matches/referee/list");
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.linkText("MI LISTA DE PARTIDOS")).click();
+		this.driver.findElement(By.linkText("Acta del partido")).click();
+		this.driver.get("http://localhost:" + this.port + "/matches/matchRecord/0/view");
+		Assert.assertEquals("¡Acceso Prohibido!", this.driver.findElement(By.xpath("//h2")).getText());
+	}
+
+	@Test
+	public void R15Positivo_RefereeEditarFinalizarPartido() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.get("http://localhost:" + this.port + "/login");
+		//this.driver.findElement(By.linkText("CONECTARSE")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("referee1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("referee1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/ul/li[3]/a/span")).click();
+		this.driver.findElement(By.linkText("Aceptar")).click();
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.linkText("MI LISTA DE PARTIDOS")).click();
+		this.driver.findElement(By.xpath("(//a[contains(text(),'Acta del partido')])[2]")).click();
+		Assert.assertEquals("Goles", this.driver.findElement(By.xpath("//table[@id='matchesTable']/thead/tr/th[2]")).getText());
+		this.driver.findElement(By.xpath("//table[@id='matchesTable']/thead/tr/th[4]")).click();
+		Assert.assertEquals("Asistencias", this.driver.findElement(By.xpath("//table[@id='matchesTable']/thead/tr/th[4]")).getText());
+		this.driver.findElement(By.linkText("+1")).click();
+		this.driver.findElement(By.linkText("+1")).click();
+		this.driver.findElement(By.linkText("+1")).click();
+		this.driver.findElement(By.linkText("+1")).click();
+		this.driver.findElement(By.xpath("(//a[contains(text(),'+1')])[2]")).click();
+		this.driver.findElement(By.xpath("(//a[contains(text(),'+1')])[2]")).click();
+		this.driver.findElement(By.xpath("//table[@id='matchesTable']/tbody/tr/td")).click();
+		Assert.assertEquals("Tomas, Vaclik", this.driver.findElement(By.xpath("//table[@id='matchesTable']/tbody/tr/td")).getText());
+		this.driver.findElement(By.xpath("//table[@id='matchesTable']/tbody/tr/td[2]")).click();
+		Assert.assertEquals("4", this.driver.findElement(By.xpath("//table[@id='matchesTable']/tbody/tr/td[2]")).getText());
+		this.driver.findElement(By.xpath("//table[@id='matchesTable']/tbody/tr/td[4]")).click();
+		this.driver.findElement(By.linkText("Editar")).click();
+		this.driver.findElement(By.id("season_start")).click();
+		this.driver.findElement(By.id("season_start")).clear();
+		this.driver.findElement(By.id("season_start")).sendKeys("2019");
+		this.driver.findElement(By.id("title")).click();
+		this.driver.findElement(By.id("result")).click();
+		this.driver.findElement(By.id("result")).clear();
+		this.driver.findElement(By.id("result")).sendKeys("Match 3 result");
+		new Select(this.driver.findElement(By.id("status"))).selectByVisibleText("PUBLISHED");
+		this.driver.findElement(By.xpath("//option[@value='PUBLISHED']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("JUGADORES")).click();
+		this.driver.findElement(By.linkText("Tomas Vaclik")).click();
+		this.driver.findElement(By.linkText("Estadísticas")).click();
+		this.driver.findElement(By.xpath("//tr[2]/td")).click();
+		Assert.assertEquals("2", this.driver.findElement(By.xpath("//tr[2]/td/b")).getText());
+		this.driver.findElement(By.xpath("//tr[2]/th")).click();
+		Assert.assertEquals("Asistencias", this.driver.findElement(By.xpath("//tr[2]/th")).getText());
+		this.driver.findElement(By.xpath("//tr[3]/td")).click();
+		Assert.assertEquals("4", this.driver.findElement(By.xpath("//tr[3]/td")).getText());
+		Assert.assertEquals("Goles", this.driver.findElement(By.xpath("//tr[3]/th")).getText());
+	}
+
+	@Test
+	public void R15Negativo_RefereeEditarFinalizarPartido() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.get("http://localhost:" + this.port + "/login");
+		//this.driver.findElement(By.linkText("CONECTARSE")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("referee1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("referee1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/ul/li[2]/a/span")).click();
+		this.driver.findElement(By.linkText("Acta del partido")).click();
+		this.driver.findElement(By.linkText("Editar")).click();
+		this.driver.findElement(By.xpath("//form[@id='add-footballPlayer-form']/div/div[3]")).click();
+		this.driver.findElement(By.id("result")).clear();
+		this.driver.findElement(By.id("result")).sendKeys("");
+		this.driver.findElement(By.xpath("//form[@id='add-footballPlayer-form']/div/div[2]")).click();
+		new Select(this.driver.findElement(By.id("status"))).selectByVisibleText("PUBLISHED");
+		this.driver.findElement(By.xpath("//option[@value='PUBLISHED']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//form[@id='add-footballPlayer-form']/div/div[3]/div")).click();
+		Assert.assertEquals("Published match records must have a result", this.driver.findElement(By.xpath("//form[@id='add-footballPlayer-form']/div/div[3]/div/span[2]")).getText());
+	}
+
+	@Test
+	public void R21Positivo_PresidenteVerEstadisticasActaActualizadas() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.get("http://localhost:" + this.port + "/login");
+		//driver.findElement(By.linkText("Conectarse")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("referee1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("referee1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//div[@id='main-navbar']/ul[2]/li/a/strong")).click();
+		this.driver.findElement(By.linkText("LISTA DE PETICIONES DE ÁRBITRO")).click();
+		this.driver.findElement(By.linkText("Aceptar")).click();
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.linkText("MI LISTA DE PARTIDOS")).click();
+		this.driver.get("http://localhost:" + this.port + "/matches/matchRecord/3/view");
+		//this.driver.findElement(By.xpath("//table[@id='matchesTable']/tbody/tr[2]/td[8]")).click();
+		//this.driver.findElement(By.xpath("(//a[contains(text(),'Acta del partido')])[2]")).click();
+		this.driver.findElement(By.linkText("+1")).click();
+		this.driver.findElement(By.linkText("+1")).click();
+		this.driver.findElement(By.xpath("(//a[contains(text(),'+1')])[2]")).click();
+		this.driver.findElement(By.linkText("Editar")).click();
+		this.driver.findElement(By.id("season_start")).click();
+		this.driver.findElement(By.id("season_start")).clear();
+		this.driver.findElement(By.id("season_start")).sendKeys("2019");
+		this.driver.findElement(By.id("result")).click();
+		this.driver.findElement(By.id("result")).clear();
+		this.driver.findElement(By.id("result")).sendKeys("Match 3 result");
+		new Select(this.driver.findElement(By.id("status"))).selectByVisibleText("PUBLISHED");
+		this.driver.findElement(By.xpath("//option[@value='PUBLISHED']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("REFEREE1")).click();
+		this.driver.findElement(By.linkText("Desconectarse")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.get("http://localhost:" + this.port + "/login");
+		//driver.findElement(By.linkText("Conectarse")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("presidente1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("presidente1");
+		this.driver.findElement(By.xpath("//form[@action='/login']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("EQUIPOS")).click();
+		this.driver.findElement(By.xpath("//table[@id='vetsTable']/tbody/tr/td/a/b")).click();
+		this.driver.findElement(By.linkText("Lista de Jugadores")).click();
+		this.driver.findElement(By.linkText("Tomas Vaclik")).click();
+		this.driver.findElement(By.linkText("Estadísticas")).click();
+		this.driver.findElement(By.xpath("//tr[2]/th")).click();
+		Assert.assertEquals("Asistencias", this.driver.findElement(By.xpath("//tr[2]/th")).getText());
+		this.driver.findElement(By.xpath("//tr[2]/td/b")).click();
+		Assert.assertEquals("1", this.driver.findElement(By.xpath("//tr[2]/td/b")).getText());
+		this.driver.findElement(By.xpath("//tr[3]/th")).click();
+		Assert.assertEquals("Goles", this.driver.findElement(By.xpath("//tr[3]/th")).getText());
+		this.driver.findElement(By.xpath("//tr[3]/td")).click();
+		Assert.assertEquals("2", this.driver.findElement(By.xpath("//tr[3]/td")).getText());
+	}
+
+	@Test
+	public void R21Negativo_PresidenteVerEstadisticasActaActualizadas() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.get("http://localhost:" + this.port + "/login");
+		//this.driver.findElement(By.linkText("Conectarse")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("presidente1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("presidente1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.linkText("LISTA DE PARTIDOS")).click();
+		this.driver.get("http://localhost:" + this.port + "/matches/matchRecord/2/view");
+		Assert.assertEquals("¡Acceso Prohibido!", this.driver.findElement(By.xpath("//h2")).getText());
 	}
 
 	@AfterEach
@@ -157,6 +384,7 @@ public class DemoUITest {
 		}
 	}
 
+	@SuppressWarnings("unused")
 	private boolean isElementPresent(final By by) {
 		try {
 			this.driver.findElement(by);
@@ -166,6 +394,7 @@ public class DemoUITest {
 		}
 	}
 
+	@SuppressWarnings("unused")
 	private boolean isAlertPresent() {
 		try {
 			this.driver.switchTo().alert();
@@ -175,6 +404,7 @@ public class DemoUITest {
 		}
 	}
 
+	@SuppressWarnings("unused")
 	private String closeAlertAndGetItsText() {
 		try {
 			Alert alert = this.driver.switchTo().alert();
