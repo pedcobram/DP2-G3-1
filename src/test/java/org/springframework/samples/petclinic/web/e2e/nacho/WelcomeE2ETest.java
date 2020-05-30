23061646ad76e8abccca1ccb731dd8deef33ec9e
ignrodflo <ignrodflo@users.noreply.github.com>
2020-05-30 18:35:14 +0200

    mysql

--- src/test/java/org/springframework/samples/petclinic/web/e2e/nacho/WelcomeE2ETest.java
+++ src/test/java/org/springframework/samples/petclinic/web/e2e/nacho/WelcomeE2ETest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -24,28 +25,28 @@ public class WelcomeE2ETest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	//	@WithMockUser(username = "manuel", authorities = {
-	//		"authenticated"
-	//	})
-	//	@Test //CASO POSITIVO - CON FAN
-	//	void testWelcomeFanSuccess() throws Exception {
-	//
-	//		this.mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("isVip")).andExpect(MockMvcResultMatchers.model().attributeExists("isFan"))
-	//			.andExpect(MockMvcResultMatchers.model().attributeExists("club")).andExpect(MockMvcResultMatchers.model().attributeExists("lastMatches")).andExpect(MockMvcResultMatchers.model().attribute("isFan", true))
-	//			.andExpect(MockMvcResultMatchers.view().name("welcome")).andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/welcome.jsp"));
-	//	}
 
+	@WithMockUser(username = "ignacio", authorities = {
+		"authenticated"
+	})
+	@Test //CASO POSITIVO - CON FAN
+	void testWelcomeFanSuccess() throws Exception {
 
-	//	@WithMockUser(username = "ignacio", authorities = {
-	//		"authenticated"
-	//	})
-	//	@Test //CASO POSITIVO - SIN FAN
-	//	void testWelcomeWithUserNoFanSuccess() throws Exception {
-	//
-	//		this.mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("isVip")).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("isFan"))
-	//			.andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("club")).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("lastMatches")).andExpect(MockMvcResultMatchers.view().name("welcome"))
-	//			.andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/welcome.jsp"));
-	//	}
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("isVip")).andExpect(MockMvcResultMatchers.model().attributeExists("isFan"))
+			.andExpect(MockMvcResultMatchers.model().attributeExists("club")).andExpect(MockMvcResultMatchers.model().attributeExists("lastMatches")).andExpect(MockMvcResultMatchers.model().attribute("isFan", true))
+			.andExpect(MockMvcResultMatchers.view().name("welcome")).andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/welcome.jsp"));
+	}
+
+	@WithMockUser(username = "manuel", authorities = {
+		"authenticated"
+	})
+	@Test //CASO POSITIVO - SIN FAN
+	void testWelcomeWithUserNoFanSuccess() throws Exception {
+
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("isVip")).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("isFan"))
+			.andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("club")).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("lastMatches")).andExpect(MockMvcResultMatchers.view().name("welcome"))
+			.andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/jsp/welcome.jsp"));
+	}
 
 	@WithAnonymousUser()
 	@Test //CASO POSITIVO - SIN FAN
